92ca5ec38d7c9751294472fb17df207fffae12c1
jaderyan <jadeelizabethryan@outlook.com>
2020-07-03 11:58:40 +0100

    increase stream count route implemented

--- controllers/index.js
+++ controllers/index.js
@@ -1,13 +1,60 @@
 const User = require('../models/user');
 
-function getUser(req, res, next) {
+async function addUser(userId) {
+  const newUser = new User({ userId, streams: 1 });
+  await newUser.save();
+
+  return newUser;
+}
+
+async function getUser(req, res, next) {
   const { userId } = req.params;
 
-  return User.findOne({ userId }).then((user) => (user ? res.send(user) : res
-    .status(404)
-    .json({ error: 'User not found' }))).catch((err) => next(err));
+  try {
+    const user = await User.findOne({ userId });
+    return (user ? res.send(user) : res
+      .status(404)
+      .json({ error: 'User not found' }));
+  } catch (err) {
+    return next(err);
+  }
+}
+
+async function startStream(req, res, next) {
+  const { userId } = req.params;
+
+  try {
+    const user = await User.findOne({ userId });
+
+    if (user) {
+      let status;
+      let data;
+
+      // only increase count if user has less than 3 streams
+      if (user.streams < 3) {
+        user.streams += 1;
+        await user.save();
+
+        status = 200;
+        data = user;
+      } else {
+        status = 403;
+        data = { error: 'Maximum number of streams reached' };
+      }
+
+      return res.status(status).send(data);
+    }
+
+    // if there is no new user create one with one active stream
+    const newUser = await addUser(userId);
+
+    return res.status(201).send(newUser);
+  } catch (err) {
+    return next(err);
+  }
 }
 
 module.exports = {
   getUser,
+  startStream,
 };
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
-const { getUser } = require('../controllers');
+const { getUser, startStream } = require('../controllers');
 
 router.route('/streams/users/:userId').get(getUser);
+
+router.route('/streams/users/:userId/start').post(startStream);
 
 module.exports = router;
